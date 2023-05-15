19670806b0840c1f0452c5e06603b2011f64b262
Nicosetzes <nicosetzes@gmail.com>
2023-05-15 00:32:52 -0300

    V7.2 From now on, FE sends the info in a better format to BE when creating a tournament, so I've adjusted the controller to reflect these changes.

--- controller/postTournaments/index.js
+++ controller/postTournaments/index.js
@@ -8,57 +8,25 @@ const postTournaments = async (req, res) => {
     const { name, format, players, teams, cloudinaryId } = req.body
 
     try {
-        // Formatting tournament for DB BEGINS //
-
         let groups
-        let teamsForDB
         let newTournament
 
         if (format == "world_cup" || format == "league_playin_playoff") {
             groups = Array.from(new Set(teams.map(({ group }) => group)))
 
-            teamsForDB = teams.map(({ id, name, value, group }) => {
-                let indexOfPlayer = players.findIndex(
-                    (player) => player.id == value
-                )
-
-                return {
-                    team: { id, name },
-                    player: {
-                        id: value,
-                        name: players[indexOfPlayer].name,
-                    },
-                    group,
-                }
-            })
-
             newTournament = await originateTournament({
                 name,
                 players,
-                teams: teamsForDB,
+                teams,
                 format,
                 groups,
                 // cloudinary_id: cloudinaryId,
             })
         } else {
-            teamsForDB = teams.map(({ id, name, value }) => {
-                let indexOfPlayer = players.findIndex(
-                    (player) => player.id == value
-                )
-
-                return {
-                    team: { id, name },
-                    player: {
-                        id: value,
-                        name: players[indexOfPlayer].name,
-                    },
-                }
-            })
-
             newTournament = await originateTournament({
                 name,
                 players,
-                teams: teamsForDB,
+                teams,
                 format,
                 // cloudinary_id: cloudinaryId,
             })
