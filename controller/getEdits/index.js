c5688b96254fde05769d55d8de0c57d0f3a259d6
Nicosetzes <nicosetzes@gmail.com>
2025-11-08 01:48:40 -0300

    V13.4 Reduced edit limit per page from 10 to 9

--- controller/getEdits/index.js
+++ controller/getEdits/index.js
@@ -3,7 +3,7 @@ const editsModel = require("./../../dao/models/edits")
 const getEdits = async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1
-        const limit = 10
+        const limit = 9
         const skip = (page - 1) * limit
 
         // Get total count for pagination info
